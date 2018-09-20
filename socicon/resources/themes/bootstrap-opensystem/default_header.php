d4c1cfe552272c1d872bb7f9498e8e28b453d9ae
Daniele Lolli (UncleDan) <uncledan@uncledan.it>
2018-09-20 12:43:20 +0200

    adding socicon

---
+++ socicon/resources/themes/bootstrap-opensystem/default_header.php
@@ -0,0 +1,3 @@
+<img style="width: 511px; height: 80px;"
+alt="OPeN SYSTEM s.r.l. - Bologna"
+src="http://www.opensystembologna.it/wp-content/themes/wp-opensystem-14-11-1/images/logo_open_system.gif">
